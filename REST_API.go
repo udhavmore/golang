12a0b51546b38910c6e20a670e3a391ace07015a
udhavmore <udhav@djangoapp.com>
2024-06-13 17:53:33 +0530

    Added function to reset the site visit counter

--- REST_API.go
+++ REST_API.go
@@ -6,8 +6,9 @@ import (
 	"strconv"
 )
 
+// Simple REST API code to count the Site Visits
 func main() {
-	// Simple REST API code to count the Site Visits
+
 	server := "localhost:8080"
 	count := 0
 	fmt.Printf("Starting Server. Listening on: http://%v", server)
@@ -15,6 +16,12 @@ func main() {
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		count++
 		msg := "Site Visits: " + strconv.Itoa(count)
+		fmt.Fprint(w, msg)
+	})
+
+	mux.HandleFunc("POST /reset", func(w http.ResponseWriter, r *http.Request) {
+		count = 0
+		msg := "Counter was reset"
 		fmt.Fprint(w, msg)
 	})
 
